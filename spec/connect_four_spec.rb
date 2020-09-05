c43142a88dba49445a92df969fa193a3ad0ad68e
Christian Ortega <christianmortega7@gmail.com>
2020-09-05 17:32:11 -0400

    Write ConnectFour#game_over? tests

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -38,4 +38,150 @@ describe ConnectFour do
       expect(subject.grid[2][5]).to eql("O")
     end
   end
+
+  describe "#game_over?" do
+    it "returns true when board is full" do
+      subject.place_chip(0, "O")
+      subject.place_chip(0, "@")
+      subject.place_chip(0, "O")
+      subject.place_chip(0, "@")
+      subject.place_chip(0, "O")
+      subject.place_chip(0, "@")
+
+      subject.place_chip(1, "O")
+      subject.place_chip(1, "@")
+      subject.place_chip(1, "O")
+      subject.place_chip(1, "@")
+      subject.place_chip(1, "O")
+      subject.place_chip(1, "@")
+
+      subject.place_chip(2, "@")
+      subject.place_chip(2, "O")
+      subject.place_chip(2, "@")
+      subject.place_chip(2, "O")
+      subject.place_chip(2, "@")
+      subject.place_chip(2, "O")
+
+      subject.place_chip(3, "O")
+      subject.place_chip(3, "@")
+      subject.place_chip(3, "O")
+      subject.place_chip(3, "@")
+      subject.place_chip(3, "O")
+      subject.place_chip(3, "@")
+
+      subject.place_chip(4, "O")
+      subject.place_chip(4, "@")
+      subject.place_chip(4, "O")
+      subject.place_chip(4, "@")
+      subject.place_chip(4, "O")
+      subject.place_chip(4, "@")
+
+      subject.place_chip(5, "@")
+      subject.place_chip(5, "O")
+      subject.place_chip(5, "@")
+      subject.place_chip(5, "O")
+      subject.place_chip(5, "@")
+      subject.place_chip(5, "O")
+
+      subject.place_chip(6, "O")
+      subject.place_chip(6, "@")
+      subject.place_chip(6, "O")
+      subject.place_chip(6, "@")
+      subject.place_chip(6, "O")
+      
+      expect(subject.place_chip(6, "@")).to eql(true)
+    end
+
+    it "returns true when four in a row" do
+      subject.place_chip(1, "@")
+      subject.place_chip(2, "@")
+      subject.place_chip(3, "@")
+      
+      expect(subject.place_chip(4, "@")).to eql(true)
+    end
+
+    it "returns true when four in a column" do
+      subject.place_chip(2, "O")
+      subject.place_chip(2, "O")
+      subject.place_chip(2, "O")
+
+      expect(subject.place_chip(2, "O")).to eql(true)
+    end
+
+    it "returns true when four in a top-left to bottom-right diagonal" do
+      subject.place_chip(2, "O")
+      subject.place_chip(3, "O")
+      subject.place_chip(4, "O")
+      subject.place_chip(5, "@")
+
+      subject.place_chip(2, "O")
+      subject.place_chip(3, "O")
+      subject.place_chip(4, "@")
+
+      subject.place_chip(2, "O")
+      subject.place_chip(3, "@")
+
+      expect(subject.place_chip(2, "@")).to eql(true)
+    end
+
+    it "returns true when four in a bottom-left to top-right diagonal" do
+      subject.place_chip(2, "@")
+      subject.place_chip(3, "O")
+      subject.place_chip(4, "O")
+      subject.place_chip(5, "O")
+
+      subject.place_chip(3, "@")
+      subject.place_chip(4, "O")
+      subject.place_chip(5, "O")
+
+      subject.place_chip(4, "@")
+      subject.place_chip(5, "O")
+
+      expect(subject.place_chip(5, "@")).to eql(true)
+    end
+
+    it "returns false when not four in a row" do
+      subject.place_chip(1, "@")
+      subject.place_chip(2, "@")
+      
+      expect(subject.place_chip(3, "@")).to eql(false)
+    end
+
+    it "returns false when not four in a column" do
+      subject.place_chip(2, "O")
+      subject.place_chip(2, "O")
+
+      expect(subject.place_chip(2, "O")).to eql(false)
+    end
+
+    it "returns false when not four in a top-left to bottom-right diagonal" do
+      subject.place_chip(2, "O")
+      subject.place_chip(3, "O")
+      subject.place_chip(4, "O")
+      subject.place_chip(5, "@")
+
+      subject.place_chip(2, "O")
+      subject.place_chip(3, "O")
+      subject.place_chip(4, "@")
+
+      subject.place_chip(2, "O")
+
+      expect(subject.place_chip(3, "@")).to eql(false)
+    end
+
+    it "returns false when not four in a bottom-left to top-right diagonal" do
+      subject.place_chip(2, "@")
+      subject.place_chip(3, "O")
+      subject.place_chip(4, "O")
+      subject.place_chip(5, "O")
+
+      subject.place_chip(3, "@")
+      subject.place_chip(4, "O")
+      subject.place_chip(5, "O")
+
+      subject.place_chip(5, "O")
+
+      expect(subject.place_chip(4, "@")).to eql(false)
+    end
+  end
 end
